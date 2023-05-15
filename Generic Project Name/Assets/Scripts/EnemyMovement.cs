430c0a7aff5f660e0ff92d22e08e93923343d6b0
Young Cheol Ko <aqr1704@gmail.com>
2023-05-14 18:26:34 -0700

    EnemyMovement(rgdbd)

--- Generic Project Name/Assets/Scripts/EnemyMovement.cs
+++ Generic Project Name/Assets/Scripts/EnemyMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    public float speed;
+    [SerializeField] Transform tragetDestination;
+    GameObject targetGameObject;
+    [SerializeField] float speed = 3.0f;
+
+    Rigidbody2D rgdbd2d;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        player = FindObjectOfType<PlayerController>().transform;
+        rgdbd2d = GetComponent<Rigidbody2D>();
+        targetGameObject = tragetDestination.gameObject;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);        
+        Vector3 direction = (tragetDestination.position - transform.position).normalized;
+        rgdbd2d.velocity = direction * speed;       
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject == targetGameObject)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack!");
+        // need to arrage with the hp system.
     }
 }
---
+++ Generic Project Name/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] Vector2 spawnArea;
+    [SerializeField] float spawnTime;
+    float timer;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            SpawnEnemy();
+            timer = spawnTime;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 position = new Vector3(
+            UnityEngine.Random.Range(-spawnArea.x,  spawnArea.x),
+            UnityEngine.Random.Range(-spawnArea.y,  spawnArea.y),
+            0.0f
+        );
+    }
+}
