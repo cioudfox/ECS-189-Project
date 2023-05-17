ad94be91c3c8939a1c80938aa92ccba96b1fa377
hongye_xu <114460759+hyexu1608@users.noreply.github.com>
2023-05-17 16:05:27 -0700

    update item controller

--- Generic Project Name/Assets/Scripts/Item/ItemController.cs
+++ Generic Project Name/Assets/Scripts/Item/ItemController.cs
@@ -7,16 +7,26 @@ public class ItemController : MonoBehaviour
     [SerializeField] public float itemDropRange;
     [SerializeField] public float itemDropSpeed;
     [SerializeField] public float itemLifetime;
+    [SerializeField] public float itemCollectSpeed;
+    [SerializeField] public float itemCollectRange;
 
-
-    Rigidbody2D item;
+    GameObject item;
     Vector3 targetDropPosition;
+    bool doneDropping;
+    GameObject player;
+
     void Start()
     {
-        this.item = this.gameObject.GetComponent<Rigidbody2D>();
+        this.item = this.gameObject;
+
         var Xrandom = Random.Range(-itemDropRange, itemDropRange);
         var Yrandom = Random.Range(-itemDropRange, itemDropRange);
         this.targetDropPosition = this.item.transform.position + new Vector3(Xrandom, Yrandom, 0.0f);
+
+        this.doneDropping = false;
+
+        this.player = GameObject.Find("Player");
+
     }
 
     // private void OnTriggerEnter2D(Collider other)
@@ -27,15 +37,34 @@ public class ItemController : MonoBehaviour
     // // Update is called once per frame.
     void Update()
     {
-        if (this.targetDropPosition != this.item.transform.position)
-        {
-            this.item.transform.position = Vector3.MoveTowards(this.item.transform.position, this.targetDropPosition, itemDropSpeed * Time.deltaTime);
-        }
+        // Debug.Log(this.player.transform.position);
+        // Debug.Log(this.item.transform.position);
 
-        this.itemLifetime -= Time.deltaTime;
-        if (this.itemLifetime < 0.0f)
+        if (!this.doneDropping)
         {
-            Destroy(this.gameObject);
+            if (this.targetDropPosition != this.item.transform.position)
+            {
+                this.item.transform.position = Vector3.MoveTowards(this.item.transform.position, this.targetDropPosition, this.itemDropSpeed * Time.deltaTime);
+            }
+            else
+            {
+                this.doneDropping = true;
+            }
+        }
+        else
+        {
+            // Item existence time
+            this.itemLifetime -= Time.deltaTime;
+            if (this.itemLifetime < 0.0f)
+            {
+                Destroy(this.gameObject);
+            }
+
+            // Item picked up by player
+            if (Vector3.Distance(this.item.transform.position, this.player.transform.position) < itemCollectRange)
+            {
+                this.item.transform.position = Vector3.MoveTowards(this.item.transform.position, this.player.transform.position, this.itemCollectSpeed * Time.deltaTime);
+            }
         }
     }
 }
