ae13a0ff59373232793d2aa23d4d988fe9bf2a25
Young Cheol Ko <aqr1704@gmail.com>
2023-06-04 23:01:36 -0700

    Delete Empty file & Complete Stage1 & Canvas adjust

--- Generic Project Name/Assets/Scripts/Enemy/BossAnimator.cs
+++ Generic Project Name/Assets/Scripts/Enemy/BossAnimator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAnimator : MonoBehaviour
+public class BossAnimator : MonoBehaviour
 {
     Animator animator;
     Transform player;
